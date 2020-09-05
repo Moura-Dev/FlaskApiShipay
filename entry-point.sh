9ba283ddd4ca000c8fefd6fbda08617c1ab76c37
Junior Moura <junior.moura19@hotmail.com>
2020-09-05 18:51:21 -0300

    a

--- entry-point.sh
+++ entry-point.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
-flask db init
-flask db migrate
-flask db upgrade
+
+until flask db upgrade
+  do
+      echo "Waiting for mysql ready..."
+      sleep 5
+      flask db init
+      flask db migrate -m 'create initial tables'
+  done
+
 flask run --host=0.0.0.0
